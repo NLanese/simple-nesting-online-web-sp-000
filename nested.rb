c756a506fd8fd1846cdf547b10c415688ee8b922
Nick Lanese <nlanese21@gmail.com>
2020-07-20 07:50:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -122,5 +122,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-	programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+	new_array = programmer_hash[:dennis_ritchie][:languages]
+	new_array << "Assembly"
+	programmer_hash[:dennis_ritchie][:languages] = new_array
+	return programmer_hash
 end
